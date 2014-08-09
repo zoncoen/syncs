a93d7d380a2aa424a35608681bc9e79868c78b2e
zoncoen <zoncoen@gmail.com>
2014-08-09 12:53:25 +0900

    Implemet sever code.

--- commands.go
+++ commands.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"github.com/codegangsta/cli"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"html/template"
 	"log"
+	"math/rand"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/codegangsta/cli"
+	"github.com/elazarl/go-bindata-assetfs"
 )
+
+type Option struct {
+	Theme    string
+	Markdown string
+	Master   bool
+}
 
 var Commands = []cli.Command{
 	commandInit,
@@ -24,6 +41,13 @@ var commandUp = cli.Command{
 	Usage: "",
 	Description: `
 `,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "theme",
+			Value: "default",
+			Usage: "style theme (default,beige,sky,night,serif,simple,solarized)",
+		},
+	},
 	Action: doUp,
 }
 
@@ -43,4 +67,48 @@ func doInit(c *cli.Context) {
 }
 
 func doUp(c *cli.Context) {
+	mdFilePath := c.Args()[0]
+	mdFileName := filepath.Base(mdFilePath)
+
+	// assets
+	http.Handle("/css/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
+	http.Handle("/js/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
+	http.Handle("/lib/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
+	http.Handle("/plugin/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, "reveal.js"}))
+	http.Handle("/assets/", http.FileServer(&assetfs.AssetFS{Asset, AssetDir, ""}))
+
+	// markdown file
+	http.HandleFunc("/"+mdFileName,
+		func(w http.ResponseWriter, r *http.Request) {
+			t, _ := template.ParseFiles(mdFilePath)
+			t.Execute(w, "")
+		})
+
+	// handle client slides
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, r *http.Request) {
+			opt := &Option{Theme: c.String("theme"), Markdown: mdFileName, Master: false}
+			indexHtml, _ := Asset("tmpl/index.html")
+			indexTemplate := template.Must(template.New("index").Parse(string(indexHtml)))
+			indexTemplate.Execute(w, opt)
+		})
+
+	// generate url of master slides
+	h := sha256.New()
+	rand.Seed(time.Now().Unix())
+	h.Write([]byte(strconv.Itoa(rand.Int())))
+	masterPath := "/" + base64.URLEncoding.EncodeToString(h.Sum(nil))
+	fmt.Println("Master: http://localhost:8080" + masterPath)
+
+	// handle master slides
+	http.HandleFunc(masterPath,
+		func(w http.ResponseWriter, r *http.Request) {
+			opt := &Option{Theme: c.String("theme"), Markdown: mdFileName, Master: true}
+			indexHtml, _ := Asset("tmpl/index.html")
+			indexTemplate := template.Must(template.New("index").Parse(string(indexHtml)))
+			indexTemplate.Execute(w, opt)
+		})
+
+	err := http.ListenAndServe(":8080", nil)
+	panic("ListenAndServe: " + err.Error())
 }
